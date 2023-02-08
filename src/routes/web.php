340c4db60a62feb3a468d716a373b11be4f67d38
Allbeiskaia <ya.1mfi@yandex.ru>
2023-02-08 12:28:08 +0700

    create project

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/cp', [AdminController::class, 'main'])->name('main');
+
+});
+
+
+Route::middleware('guest')->group(function () {
+    Route::get('/cp', [AuthController::class, 'loginForm'])->name('login');
+    Route::post('/cp', [AuthController::class, 'login'])->name('login_process');
+
+});
+
+
